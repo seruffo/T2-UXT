44d02f627fd0523eb2e877196d1706f4717da2de
Kennedy Edson Silva de Souza <kennedy.es.souza@outlook.com>
2020-02-05 18:36:11 -0300

    Revert "Evento de mouse removidos"
    This reverts commit 16b7593f07fa652838ad454c601676956b10f0c9.

--- client/searchgazer.js
+++ client/searchgazer.js
@@ -10424,8 +10424,8 @@ if (typeof exports !== 'undefined') {
     var addMouseEventListeners = function() {
         //third argument set to true so that we get event on 'capture' instead of 'bubbling'
         //this prevents a client using event.stopPropagation() preventing our access to the click
-        //document.addEventListener('click', clickListener, true);
-        //document.addEventListener('mousemove', moveListener, true);
+        document.addEventListener('click', clickListener, true);
+        document.addEventListener('mousemove', moveListener, true);
     };
 
     /**
